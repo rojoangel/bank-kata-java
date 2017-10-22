1724c73876d4f4145836596ef926e88b25b7566a
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-10-22 23:30:01 +0200

    statements should print deposit

--- src/test/java/kata/bank/StatementsTest.java
+++ src/test/java/kata/bank/StatementsTest.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -25,5 +28,14 @@ public class StatementsTest {
     public void should_print_header() {
         statements.printTo(printer);
         verify(printer).printLine(Statements.HEADER);
+    }
+
+    public void should_print_deposit() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse("01/04/2014", formatter);
+
+        statements.add(new Transaction(date, 1000.00), 1000.00);
+        statements.printTo(printer);
+        verify(printer).printLine("01/04/2014 | 1000.00 | 1000.00");
     }
 }
